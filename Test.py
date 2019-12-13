48f959d0647a483fdb6ae7f0efb772f52b0de595
YReisner <45455842+YReisner@users.noreply.github.com>
2019-12-13 17:48:58 +0200

    Basic image loading code - to be improved

--- Test.py
+++ Test.py
@@ -2,3 +2,22 @@
 This is a test
 This also
 """
+Params= GetDefaultParameters()
+
+Params= GetDefaultParameters()
+
+DandL= GetData(Params[‘Data’])
+
+SplitData= TrainTestSplit( DandL[‘Data’], DandL[‘Labels’], Params[‘Split’])
+				 # returns train data, test data, train labels and test labels
+
+TrainDataRep = Prepare( SplitData[‘Train’][‘Data’], Params[‘Prepare’])
+
+Model =  Train(TrainDataRep, SplitData[‘Train’][‘Labels’] , Params[‘Train’])
+TestDataRep = Prepare(SplitData[‘Test’][‘Data’], Params[‘Preapare’])
+
+Results = Test(Model, TestDataRep)
+
+Summary = Evaluate(Results, SplitData[‘Test’][‘Labels’], Params[‘Summary’])
+
+ReportResults(Summary, Params[‘Report’])
