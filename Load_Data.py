29a347700abf7a31300e503f09f57c3051b14276
Yreisner <avichai@bigvu.tv>
2019-12-23 15:58:35 +0200

    Changed quite a lot of small things, mainly to make the code run.
    Still has a lot of redundancies, and small changes and additions to be made.
    
    The main of which is the hyper parameter tuning function. Some params as well.

--- Load_Data.py
+++ Load_Data.py
@@ -10,23 +10,25 @@ import pickle
 
 
 
-path = r'C:\Users\gilei\Desktop\comp\Computer_Vision_Classic-master\101_ObjectCategories'
+path = r'C:\Users\BIGVU\Desktop\Yoav\University\101_ObjectCategories'
 files = os.listdir(path)[0:3]
 
-labels = []
-img_list = []
+labels = []  # defining a list of labels
+img_list = []  # defining a list of images
+# running on all  files and all images in every file
 for file in files:
-    newPath = path+"\\"+file
-    for img in glob.glob(newPath+"\\*.jpg"):
-        raw = cv2.imread(img, 0)
+    newPath = path + "\\" + file
+    images = glob.glob(newPath + "\\*.jpg")
+    if len(images) >= 40:
+        amount = 40
+    else:
+        amount = len(images)
+    for img in images[:amount]:
+        raw = cv2.imread(img, 0)  # returns an an array of the image in gray scale
         im_data = np.asarray(raw)
-        #gray = cv2.cvtColor(im_data, cv2.COLOR_BGR2GRAY)
-        sized = cv2.resize(im_data,(200,200))
-        img_list.append(sized)
-        labels.append(file)
-print(type(img_list))
-array = np.array(img_list)
-print(type(array))
+        sized = cv2.resize(im_data, (100,100))  # resizing the data
+        img_list.append(sized) # add it to list of the images
+        labels.append(file)   # add the image label
 """ For Testing
 print(array.shape)
 print(len(labels))
@@ -34,6 +36,8 @@ print(labels[50:60])
 plt.imshow(array[400, :, :], cmap='gray')
 plt.show()
 """
-
+print(len(img_list[1][1]))
+plt.imshow(img_list[1], cmap='gray')
+plt.show()
 
 
--- Test.py
+++ Test.py
@@ -7,13 +7,13 @@ import glob
 import numpy as np
 import sklearn as sk
 import pickle
-from sklearn.cluster import MiniBatchKMeans
+from sklearn.cluster import KMeans
 import pandas as pd
 from sklearn.svm import SVC
 from sklearn.metrics import confusion_matrix
 
 def GetDefaultParameters():
-    image_size = (200,200)
+    image_size = (100,100)
     split = 0.2
     clusters = 500
     svm_c = 1
@@ -33,9 +33,9 @@ def load_data(path,imsize):
     :return: a dictionary of images raw data and its labels
     '''
 
-    files = os.listdir(path)[0:3] #get the first 10 files into files
-    labels = [] # defining a list of labels
-    img_list = [] # defining a list of images
+    files = os.listdir(path)[0:2] #get the first 10 files into files
+    labels = []  # defining a list of labels
+    img_list = []  # defining a list of images
     # running on all  files and all images in every file
     for file in files:
         newPath = path + "\\" + file
@@ -45,14 +45,14 @@ def load_data(path,imsize):
         else:
             amount = len(images)
         for img in images[:amount]:
-            raw = cv2.imread(img, 0) # returns an an array of the image in gray scale
+            raw = cv2.imread(img, 0)  # returns an an array of the image in gray scale
             im_data = np.asarray(raw)
-            sized = cv2.resize(im_data, imsize) # resizing the data
+            sized = cv2.resize(im_data, imsize)  # resizing the data
             img_list.append(sized) # add it to list of the images
             labels.append(file)   # add the image label to th list of labels
-    img_array = np.array(img_list) # transfer the list to np.array
-    data = {'Data':img_array,"Labels":labels} # create a dict of images data and the labels
-    return data
+    #img_array = np.array(img_list) # transfer the list to np.array
+    #data = {'Data':img_array,"Labels":labels} # create a dict of images data and the labels
+    return img_list,labels
 
 def train_test_split(data, labels, ratio):
     '''
@@ -65,8 +65,8 @@ def train_test_split(data, labels, ratio):
     #We need to change this into a simply 20 - 20 split, instead of the function
     unique_labels = set(labels)
     count = [(item,labels.count(item)) for item in unique_labels] # This gives me the amount of images for each category
-    img_amount = dict(count) # This allows a convenient access to the image count
-    counter = 0 # Helper variable to go over the data vector
+    img_amount = dict(count)  # This allows a convenient access to the image count
+    counter = 0  # Helper variable to go over the data vector
     X_train = []
     y_train = []
     X_test = []
@@ -77,7 +77,7 @@ def train_test_split(data, labels, ratio):
         for num in range(20):
             X_train.append(data[counter])
             y_train.append(labels[counter])
-            counter += counter
+            counter = counter + 1
         # Check if we have 40 images for this category
         if img_amount[labels[counter]] == 40:
             img_left = 20  # If we do, we should have exactly 20 images left to collect
@@ -86,11 +86,8 @@ def train_test_split(data, labels, ratio):
         for num in range(img_left):
             X_test.append(data[counter])
             y_test.append(labels[counter])
-            counter += counter
+            counter = counter + 1
 
-
-
-        
     #X_train, X_test, y_train, y_test = sk.model_selection.train_tdest_split(data, labels, test_size=ratio, random_state=42)
     split_dict = {"Train": {'Data': X_train, 'Labels': y_train}, "Test": {'Data': X_test, 'Labels': y_test}}
     return split_dict
@@ -103,7 +100,7 @@ def train_kmeans(data, params):
     :param params:
     :return:
     '''
-    sift_vec = [] # define a list of sift
+    sift_vec = []  # define a list of sift
     for img in data:
         sift = cv2.xfeatures2d.SIFT_create() #############################
         step_size = params['step'] #use the step size as defined in params
@@ -112,24 +109,17 @@ def train_kmeans(data, params):
         points, sifts = sift.compute(img, kp) #compute sifts from key points
         sift_vec.append(sifts) # add the sift to the sifts list
 
-    all_sifts_array = np.array(sift_vec)  # transfer the list to np.array
+    # transfer the list to np.array
+    all_sifts_array = list(sift_vec[0])
+    for value in sift_vec[1:]:
+        all_sifts_array = np.append(all_sifts_array, value, axis=0)
     # compute and return k_means
-    model = MiniBatchKMeans(n_clusters=params["clusters"], random_state=42)   #define the kmeans model parameters
+    model = KMeans(n_clusters=params["clusters"],  random_state=42)   # define the kmeans model parameters
     kmeans = model.fit(all_sifts_array) # fit the model on the sift and compute the kmeans
 
     return kmeans
 
-def train(data, labels, params):
-    '''
-    Train the model with SVM
-    :param data:
-    :param labels:
-    :param params:
-    :return:
-    '''
-    svm = SVC(C=params['svm_c'], kernel=params['kernel'], gamma=params['gamma']) # define the SVM parameters
-    svm.fit(data, labels) #fitting the SVM on the data
-    return svm
+
 
 def prepare(kmeans, data, labels, params):
     '''
@@ -154,14 +144,28 @@ def prepare(kmeans, data, labels, params):
         img_hist, bin_size = np.histogram(img_predicts, bins=params['bins']) #compute histogram for each image's sifts by 'bins' parameter
         hist_vec.append(img_hist) # add the histogram to histograms vector
         label_vec.append(label) # add the label to labels vector
-        img_hist = img_hist.reshape(1, 300) #reshape the size of each histogram of each image
+        img_hist = img_hist.reshape(1, params['bins']) #reshape the size of each histogram of each image
         # fill the histogram dictionary with labels and historams
         if label in hist_dict:
             hist_dict[label] = np.append(hist_dict[label], img_hist, axis=0)
         else:
             hist_dict[label] = img_hist
-        datarep = {"hists":hist_vec,"labels":label_vec} #####################
-        return datarep
+    print(len(hist_vec))
+    print(len(hist_vec[0]))
+    datarep = {"hists":hist_vec,"labels":label_vec} #####################
+    return datarep
+
+def train(data, labels, params):
+    '''
+    Train the model with SVM
+    :param data:
+    :param labels:
+    :param params:
+    :return:
+    '''
+    svm = SVC(C=params['svm_c'], kernel=params['kernel'], gamma=params['gamma']) # define the SVM parameters
+    svm.fit(data, labels)  # fitting the SVM on the data
+    return svm
 
 def test(model,test_data):
     '''
@@ -194,21 +198,21 @@ def conf_matrix(true_lable, predicted_lable):
 
 path = r'C:\Users\BIGVU\Desktop\Yoav\University\101_ObjectCategories'
 
-params= GetDefaultParameters()
+params = GetDefaultParameters()
 
-DandL = load_data(path,params['imsize'])
+data,labels = load_data(path,params['imsize'])
 
-SplitData= train_test_split( DandL['Data'], DandL['Labels'], params['Split'])
+SplitData = train_test_split(data, labels, params['Split'])
 # returns train data, test data, train labels and test labels
 
 kmeans_model = train_kmeans(SplitData['Train']['Data'], params['prepare'])
-TrainDataRep = prepare( SplitData['Train']['Data'], params['Prepare'])
+TrainDataRep = prepare(kmeans_model, SplitData['Train']['Data'],SplitData['Train']['Labels'], params['prepare'])
 # call make_hist on train data
-Model =  train(TrainDataRep, SplitData['Train']['Labels'] , params['Train'])
-TestDataRep = prepare(SplitData['Test']['Data'], params['Prepare'])
+Model = train(TrainDataRep["hists"], SplitData['Train']['Labels'], params['train'])
+TestDataRep = prepare(kmeans_model, SplitData['Test']['Data'], SplitData['Test']['Labels'], params['prepare'])
 
 Results = test(Model, TestDataRep)
 
-Summary = evaluate(Results, SplitData['Test']['Labels'], params['Summary'])
+Summary = evaluate(Results, SplitData['Test']['Labels'], [])
 
-reportResults(Summary, params['Report'])
+reportResults(Summary,[])
