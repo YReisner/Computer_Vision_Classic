6a0467b81bd2875e239def07e8ea0ccd890b99f1
gileibiker <gileibiker@gmail.com>
2019-12-28 01:28:04 +0200

    cleaned the code added documentation the code

--- Test.py
+++ Test.py
@@ -15,6 +15,10 @@ import matplotlib.pyplot as plt
 from more_itertools import unique_everseen
 
 def GetDefaultParameters():
+    '''
+    Create a dictionary of parameters which will be used along the process
+    :return: Dictionary of parameters
+    '''
     class_indices = [10,11,12,13,14,15,16,17,18,19]
     image_size = (150,150)
     split = 0.2
@@ -34,8 +38,8 @@ def load_data(path,params):
     '''
     Loads the data
     :param path: data location on the PC
-    :param imsize: desired size measure for all images
-    :return: a dictionary of images raw data and its labels
+    :param params: desired size measure for all images
+    :return: list of images raw data and list of its labels
     '''
 
     files = os.listdir(path)  # get the files
@@ -56,22 +60,18 @@ def load_data(path,params):
             sized = cv2.resize(im_data, params["image_size"])  # resizing the data
             img_list.append(sized) # add it to list of the images
             labels.append(file)   # add the image label to th list of labels
-    #img_array = np.array(img_list) # transfer the list to np.array
-    #data = {'Data':img_array,"Labels":labels} # create a dict of images data and the labels
     print("Data loading complete!")
     return img_list,labels
 
 
-def train_test_split(data, labels, ratio):
+def train_test_split(data, labels):
     '''
-    Splits the data and labels according to a ratio defined in Params.
-    :param data:
-    :param labels:
-    :param ratio:
+    Splits the data and labels.
+    :param data: list of images raw data
+    :param labels: list images's labels
     :return: Dictionary of train (data and labels) and test (data and labels)
     '''
-    #We need to change this into a simply 20 - 20 split, instead of the function
-    unique_labels = set(labels)
+    unique_labels = set(labels) # get a vctor of the unique labels
     count = [(item,labels.count(item)) for item in unique_labels] # This gives me the amount of images for each category
     img_amount = dict(count)  # This allows a convenient access to the image count
     counter = 0  # Helper variable to go over the data vector
@@ -96,21 +96,21 @@ def train_test_split(data, labels, ratio):
             y_test.append(labels[counter])
             counter = counter + 1
 
-    #X_train, X_test, y_train, y_test = sk.model_selection.train_tdest_split(data, labels, test_size=ratio, random_state=42)
+    #X_train, X_test, y_train, y_test = sk.model_selection.train_tdest_split(data, labels, random_state=42)
     split_dict = {"Train": {'Data': X_train, 'Labels': y_train}, "Test": {'Data': X_test, 'Labels': y_test}}
     return split_dict
 
 
 def train_kmeans(data, params):
     '''
-
-    :param data:
-    :param params:
-    :return:
+    Apply kmeans algorithm on the train data
+    :param data: The train data matrix
+    :param params: dictionary of parameters
+    :return: kmeans model
     '''
     sift_vec = []  # define a list of sift
     for img in data:
-        sift = cv2.xfeatures2d.SIFT_create() #############################
+        sift = cv2.xfeatures2d.SIFT_create() #create sifts
         step_size = params['step_size'] #use the step size as defined in params
         kp = [cv2.KeyPoint(x, y, step_size) for y in range(0, img.shape[0], step_size) for x in
               range(0, img.shape[1], step_size)] # compute key points
@@ -131,16 +131,16 @@ def train_kmeans(data, params):
 
 def prepare(kmeans, data, params):
     '''
-
-    :param kmeans:
-    :param data:
-    :param labels:
-    :param params:
-    :return:
+    create and compute sifts from all images and compute histograms sift of al
+    compute histogram for each image's sifts
+    :param kmeans: kmeans train model
+    :param data: The train data matrix
+    :param params: dictionary of parameters
+    :return: histograms vector of all images sifts
     '''
     hist_vec = []  # define a vector of histograms
     for img in data:  # run on the org_data tuple
-        sift = cv2.xfeatures2d.SIFT_create() #############################
+        sift = cv2.xfeatures2d.SIFT_create() # create sifts
         step_size = params['step_size'] #use the step size as defined in params
         kp = [cv2.KeyPoint(x, y, step_size) for y in range(0, img.shape[0], step_size) for x in
                 range(0, img.shape[1], step_size)]  # compute key points
@@ -156,10 +156,10 @@ def prepare(kmeans, data, params):
 def train(data, labels, params):
     '''
     Train the model with SVM
-    :param data:
-    :param labels:
-    :param params:
-    :return:
+    :param data: the train data
+    :param labels: the train labels
+    :param params:dictionary of parameters
+    :return: the computed SVM of the trained data
     '''
     svm = SVC(C=params['svm_c'], kernel=params['kernel'], gamma=params['gamma'],degree=params['degree'],probability = True,random_state=42) # define the SVM parameters
     svm.fit(data, labels)  # fitting the SVM on the data
@@ -169,22 +169,30 @@ def train(data, labels, params):
 
 def test(model,test_data):
     '''
-
-    :param model:
-    :param test_data:
-    :return:`q
+    Test the new set of data with the chosen model
+    :param model: the SVM model
+    :param test_data: vector of histograms
+    :return: The model predictions and probabilities of each image in the test data
     '''
     predictions = model.predict(test_data) #computing the predictions from the model
-    probabilities = model.predict_proba(test_data)
+    probabilities = model.predict_proba(test_data) # computing the probabilities
 
     return predictions,probabilities
 
 
-def evaluate(predicts, probabilities, real, params):
+def evaluate(predicts, probabilities, real):
+    '''
+    Evaluate the test errors, confusion matrix and find the indexes of
+    2 images with highest error (if exist) of each class in the test data
+    :param predicts: the predicted labels of the test data
+    :param probabilities: the probabilities of each
+    :param real: the real labels of test data
+    :return: the errors between the predictions and real data and the confusion matrix
+    '''
 
     error = 1 - sk.metrics.accuracy_score(predicts, real) # Compute error
 
-    cnf_mat = confusion_matrix(real, predicts, list(unique_everseen(real)),normalize=True)  # Create confusion matrix
+    cnf_mat = confusion_matrix(real, predicts, list(unique_everseen(real)))  # Create confusion matrix
 
     # Create helper dict to access correct column in probabilities
     class_ind_dict = {real[0]:0}
@@ -194,11 +202,11 @@ def evaluate(predicts, probabilities, real, params):
             class_ind_dict[real[i]] = count
             count += 1
 
-    images_loc = []
+    images_loc = [] # create an array which will contain the indexes of the worst images of each class
 
     # Let's find the two worst pictures
     temporary_count = 3  # Help Variable
-
+    # set up the first two images to be the worsts
     if probabilities[0][class_ind_dict[real[0]]] > probabilities[1][class_ind_dict[real[1]]]:
         lowest = probabilities[1][class_ind_dict[real[1]]]
         second = probabilities[0][class_ind_dict[real[0]]]
@@ -209,6 +217,7 @@ def evaluate(predicts, probabilities, real, params):
         second = probabilities[1][class_ind_dict[real[1]]]
         lowest_loc = 1
         second_loc = 2
+    # lets run on all images in order to find the 2 images with the biggest errors(if exist)  of each class
     for i in list(range(2,len(real))):
         if real[i] == real[i-1]:
             if probabilities[i][class_ind_dict[real[i]]] < lowest:
@@ -239,19 +248,26 @@ def evaluate(predicts, probabilities, real, params):
                 second = probabilities[i+1][class_ind_dict[real[i+1]]]
                 lowest_loc = 1
                 second_loc = 2
-    images_loc.append(lowest_loc + 20)
-    images_loc.append(second_loc + 20)
-    print(images_loc)
-
+    images_loc.append(lowest_loc + 20) # locations of the images with biggest error in each class in the test group
+    images_loc.append(second_loc + 20) # locations of the images with second biggest error in each class in the test group
+    print(images_loc) # print the locations array of the 2 images with the biggest errors(if exist) of each class in the test group
 
     return error,cnf_mat
 
 
-def reportResults(error,conf,labels):
+def reportResults(error,conf):
+    '''
+    prints the test error and confusion matrix
+    :param error: the error between the predictions and real
+    :param conf: confusion matrix
+    :return:
+    '''
 
     print(error)
     print(conf)
     return
+
+# visualization of the confusion matrix
 '''
     df_cm = pd.DataFrame(conf, index=list(unique_everseen(labels)), columns=list(unique_everseen(labels)),)
     col_sum = df_cm.sum(axis=1)
@@ -268,6 +284,14 @@ def reportResults(error,conf,labels):
 
 
 def validation(params,param_to_validate,possible_values):
+    '''
+    execution of the validation process
+    :param params: dictionary of parameters
+    :param param_to_validate: a hyper parameter which we want to examine
+    :param possible_values: the hyper parameter range of values we wants to examine
+    :return:
+    '''
+
 
     # Help variables to garner errors for each iteration
     train_errors = []
@@ -277,13 +301,13 @@ def validation(params,param_to_validate,possible_values):
         params[param_to_validate] = value  # Change default value of tuned parameter to the one we want to check
         params['bins'] = value
 
-        data, labels = load_data(path, params)
-        SplitData = train_test_split(data, labels, params['Split'])
-        # returns train data, test data, train labels and test labels
+        data, labels = load_data(path, params) #loads the data
+
+        SplitData = train_test_split(data, labels) # returns train data, test data, train labels and test labels
 
         kmeans_model = train_kmeans(SplitData['Train']['Data'], params)
+
         TrainDataRep = prepare(kmeans_model, SplitData['Train']['Data'], params)
-        # call make_hist on train data
 
         Model = train(TrainDataRep, SplitData['Train']['Labels'], params)
         train_predicts = Model.predict(TrainDataRep) # Predict on the training set
@@ -296,8 +320,8 @@ def validation(params,param_to_validate,possible_values):
 
         Results, probabilities = test(Model, TestDataRep)
 
-        Error, conf_mat = evaluate(Results,probabilities, SplitData['Test']['Labels'], [])
-        reportResults(Error, conf_mat, [])
+        Error, conf_mat = evaluate(Results,probabilities, SplitData['Test']['Labels'])
+        reportResults(Error, conf_mat)
 
         val_errors.append(Error) # Append the current validation error for the current value
 
@@ -315,28 +339,30 @@ def validation(params,param_to_validate,possible_values):
 
 np.random.seed(42)
 
-path = r'C:\Users\BIGVU\Desktop\Yoav\University\101_ObjectCategories'
+path = r'C:\Users\gilei\Desktop\comp\Computer_Vision_Classic-master\101_ObjectCategories'
 
 params = GetDefaultParameters()
 
-if not params['validate']:
+if not params['validate']: # in case of validation check isnt required perform the following
 
-    data,labels = load_data(path,params)
+    data,labels = load_data(path,params) #loads the data
 
-    SplitData = train_test_split(data, labels, params['Split'])
-# returns train data, test data, train labels and test labels
+    SplitData = train_test_split(data, labels)# returns train data, test data, train labels and test labels
 
-    kmeans_model = train_kmeans(SplitData['Train']['Data'], params)
-    TrainDataRep = prepare(kmeans_model, SplitData['Train']['Data'], params)
-# call make_hist on train data
-    Model = train(TrainDataRep, SplitData['Train']['Labels'], params)
-    TestDataRep = prepare(kmeans_model, SplitData['Test']['Data'], params)
+    kmeans_model = train_kmeans(SplitData['Train']['Data'], params) # compute kmeans on the train data
 
-    Results,probabilities = test(Model, TestDataRep)
+    TrainDataRep = prepare(kmeans_model, SplitData['Train']['Data'], params) # return computed histograms out of train data's sifts using the kmeans model
 
-    Error,conf_mat = evaluate(Results,probabilities, SplitData['Test']['Labels'], [])
+    Model = train(TrainDataRep, SplitData['Train']['Labels'], params) # perform the SVM (Linear or RBF) on the train historgrams
 
-    reportResults(Error,conf_mat,SplitData['Test']['Labels'])
+    TestDataRep = prepare(kmeans_model, SplitData['Test']['Data'], params) # return computed histograms out of test data's sifts using the kmeans model
+
+    Results,probabilities = test(Model, TestDataRep) # perform the SVM (Linear or RBF) on the test historgrams, returns the predictions and probabilities
+
+    Error,conf_mat = evaluate(Results,probabilities, SplitData['Test']['Labels']) # evaluate the error and the confusion matrix.
+                        # and also prints a list of the locations of the two largest errors on images of each class
+
+    reportResults(Error,conf_mat) #prints the error and the confusion matrix
 
 else:
-    validation(params,'clusters',(40,80,100,150,200,250,300,350,400,450,500,550,600))
+    validation(params,'clusters',(40,80,100,150,200,250,300,350,400,450,500,550,600)) #in case of validation check is required
